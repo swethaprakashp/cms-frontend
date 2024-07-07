c3bf030c9c503ccba80cf6999aeebff3c56908ad
Swetha Prakash P <swetha.prakash@minusbugs.com>
2024-07-07 19:31:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/cms-frontend" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
