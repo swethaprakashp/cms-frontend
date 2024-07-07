f12f73bd1631b0df2cae3ddfd55364976d1e100f
Swetha Prakash P <swetha.prakash@minusbugs.com>
2024-07-07 22:32:59 +0530

    redirecting issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { BrowserRouter , Route, Routes, useHistory } from 'react-router-dom';
 import Home from './pages/home'
 import Dashboard from './pages/dashboard';
 const App = () => {
+  function RedirectHandler() {
+    const history = useHistory();
+  
+    useEffect(() => {
+      const redirectPath = sessionStorage.getItem('redirect');
+      sessionStorage.removeItem('redirect');
+      if (redirectPath) {
+        history.replace(redirectPath);
+      }
+    }, [history]);
+  
+    return null;
+  }
+  
   return (
     <BrowserRouter basename="/cms-frontend"> 
+    <RedirectHandler />
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
